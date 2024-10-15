79617a221cf8ddd41e311c12eecedfb700d9581d
Victorwasonga <117116140+Victorwasonga@users.noreply.github.com>
2024-10-15 21:12:55 +0300

    Update start_container.sh
    updated start container.sh file

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull victorwasonga/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+Docker run -d -p 5000:5000 victorwasonga/simple-python-flask-app
