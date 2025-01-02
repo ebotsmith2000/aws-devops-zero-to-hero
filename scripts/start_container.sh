6bf2fd143a99b6b83812f3b2b3b3f154b5096ec4
ebotsmith2000 <ebotsmith@gmail.com>
2025-01-02 08:26:59 -0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull docker push ebotsmith/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 docker push ebotsmith/simple-python-flask-app
+docker run -d -p 5001:5001 docker push ebotsmith/simple-python-flask-app
